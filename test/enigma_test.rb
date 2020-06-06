e76130d9d4d0d2ab4fa6e12f1dac35afe09dad6f
Chandler Hulstrom <chulstro11@gmail.com>
2020-06-06 16:14:19 -0600

    Intergrated shift class into enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,1 +1,14 @@
 require_relative "test_helper"
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @message = Enigma.new
+  end
+
+  def test_it_can_encrypt
+    @message.encrypt("Fight me")
+    assert_equal "a", Enigma.characters[0]
+  end
+end
